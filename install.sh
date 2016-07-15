12fd1231c51b9e3135b2bae753d6342c59d8f1c9
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2016-07-14 23:03:53 -0300

    Add docker installation

--- install.sh
+++ install.sh
@@ -51,6 +51,25 @@ if has_not google-chrome-stable; then
 fi
 msg_ok "chrome stable"
 
+# Docker
+if has_not docker; then
+	sudo apt-get update
+	sudo apt-get install apt-transport-https ca-certificates -y
+	sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D -y
+
+	echo deb https://apt.dockerproject.org/repo ubuntu-wily main | sudo tee /etc/apt/sources.list.d/docker.list
+	sudo apt-get update
+	sudo apt-get purge lxc-docker -y
+	apt-cache policy docker-engine
+	sudo apt-get install linux-image-extra-$(uname -r) -y
+	sudo apt-get update
+	sudo apt-get install docker-engine -y
+	sudo service docker start
+	sudo groupadd docker
+	sudo usermod -aG docker iago
+fi
+msg_ok "docker"
+
 # Java
 if has_not_dir "$HOME/.java"; then
 	sudo add-apt-repository -y ppa:webupd8team/java
